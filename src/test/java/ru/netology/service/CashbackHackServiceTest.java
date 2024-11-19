b22ea4d3849f544fab9cf00b1dcaa7a6a110be29
Igor Eliseev <eliseev.igor2207@gmail.com>
2024-11-19 07:31:02 +0500

    added CashbackHackServiceTest

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,33 +1,33 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainBelow1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 950;
         int actual = service.remain(amount);
         int expected = 50;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemain1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainAbove1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1050;
         int actual = service.remain(amount);
         int expected = 950;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
